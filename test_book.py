0a066ccbac891df41ba11e88db94ea5d617ff156
Marcel Suter <marcel.suter@bzz.ch>
2023-08-29 14:33:48 +0200

    Update test_book.py

--- test_book.py
+++ test_book.py
@@ -1,18 +1,1 @@
-def sample_book1():
-    assert False
 
-
-def sample_book2():
-    assert False
-
-
-def test_init():
-    assert False
-
-
-def test_init_wrong():
-    assert False
-
-
-def test_data():
-    assert False
