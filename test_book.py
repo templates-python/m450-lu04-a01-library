3ab0dd4790fb99a703fd5ba59326768c8a87f18e
Marcel Suter <marcel.suter@bzz.ch>
2024-02-07 06:59:56 +0100

    added test functions

--- test_book.py
+++ test_book.py
@@ -1,1 +1,29 @@
+def test_init_correct():
+    """
+    create a book with title and isbn
+    """
+    assert False
 
+def test_init_wrong():
+    """
+    create a book with one value or none
+    """
+    assert False
+
+def test_output():
+    """
+    check the output when printing a book
+    """
+    assert False
+
+def test_location_1():
+    """
+    is location empty when creating a book with title and isbn
+    """
+    assert False
+
+def test_location_2():
+    """
+    is location empty when creating a book with title, isbn and location
+    """
+    assert False
